f729a251142a9da37fde4373943c22d142e84219
Mampionona <devke03@gmail.com>
2019-04-04 16:36:41 +0300

    sauvegardes

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ jQuery(function($) {
   $('.avantage-item').each(function(i, item) {
     var widget_container = $(item).find('.elementor-widget-container');
     widget_container.addClass('d-flex flex-wrap justify-content-between');
-    $('<div />', { Class: 'bg-blue numero text-center', html: i + 1}).prependTo(widget_container);
+    $('<div />', { Class: 'bg-blue numero text-center', html: i + 1 }).prependTo(widget_container);
   });
 
   function appendModificationLabel() {
