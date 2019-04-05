440afe0d75401b6de841f5874fa2ecbf75c2d415
Mampionona <devke03@gmail.com>
2019-04-05 13:29:43 +0300

    page 404

--- 404.php
+++ 404.php
@@ -15,15 +15,21 @@ get_header();
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-			<div class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentynineteen' ); ?></h1>
-				</header><!-- .page-header -->
+			<div class="error-404 not-found text-center mt-5 mb-5">
+				<div class="container">
+					<div class="row">
+						<div class="col">
+							<header class="page-header">
+								<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentynineteen' ); ?></h1>
+							</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentynineteen' ); ?></p>
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
+							<div class="page-content">
+								<p><?php _e('Apparemment, rien n\'a été trouvé à cette adresse.', 'vitanova'); ?></p>
+								<?php // get_search_form(); ?>
+							</div><!-- .page-content -->
+						</div>
+					</div>
+				</div>
 			</div><!-- .error-404 -->
 
 		</main><!-- #main -->
